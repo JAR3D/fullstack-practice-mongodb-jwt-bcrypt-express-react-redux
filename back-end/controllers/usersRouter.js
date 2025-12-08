4b7cf335c671edc89e24c35ec5780d07a0c82269
Joao Rosado <joaorosado@Joaos-MacBook-Air-2.local>
2025-12-08 18:15:52 +0000

    feat: add jwt-based login endpoint

--- back-end/controllers/usersRouter.js
+++ back-end/controllers/usersRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user.js';
+import { JWT_SECRET } from '../utils/config.js';
 
 const usersRouter = express.Router();
 
@@ -28,6 +30,26 @@ usersRouter.post('/', async (req, res) => {
     const savedUser = await user.save();
 
     res.status(201).json(savedUser);
+});
+
+usersRouter.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    const isPasswordValid = !user
+        ? false
+        : await bcrypt.compare(password, user.passwordHash);
+
+    if (!(user && isPasswordValid)) {
+        return res.status(401).json({ error: 'invalid username or password' });
+    }
+
+    const userForToken = { username, id: user.id };
+
+    const token = jwt.sign(userForToken, JWT_SECRET, { expiresIn: 60 * 60 });
+
+    res.status(200).json({ token, username, name: user.name });
 });
 
 export default usersRouter;
